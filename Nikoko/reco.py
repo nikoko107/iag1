0303173ca880b50a6d201f87a2674beac990a67e
Unknown <nicolas.dunand@gmail.com>
2018-02-04 15:52:08 +0100

    aa
    bbbvbvb

--- Nikoko/reco.py
+++ Nikoko/reco.py
@@ -9,8 +9,6 @@ datarat = pd.read_csv('ratings.csv')
 X = datarat.iloc[:, :-2].values
 y = datarat.iloc[:, -2].values
 
-# Diviser le dataset entre le Training set et le Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-
+# Dviser le dataset entre le Training set et le Test set
+#from sklearn.model_selection import train_test_split
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
